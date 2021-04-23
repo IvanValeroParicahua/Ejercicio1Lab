5be13dd465f182c2287e3e16d99c5ef8e51556ee
yoset <ycozco@unsa.edu.pe>
2021-04-23 10:58:34 -0500

    funcion de sumar y restar agregadas

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,11 @@
 
 public class Calculator {
-
+	public static void main(String []args){
+     }
+     public static int sumar(int a, int b){
+           return a+b;
+       }
+     public static int restar(int a, int b){
+     	return a - b;
+     }
 }
